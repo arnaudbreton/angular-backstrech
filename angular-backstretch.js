8ddbe69ec07df2dcd971a6023839e7942fc9c41c
Erik Zettersten <Erik@Zettersten.com>
2014-04-06 15:15:20 -0400

    Updated to version 1.0.0
    - Fixes an issue with a missing comma
    - Throws errors when dependencies are missing
    - Uses correct API when the body element is the context element
    - Changes API to use the directive attr as opposed to its own attr (no more background-url)
    - Added "ng" in front of directive. This is a design trend most devs are following.

--- angular-backstretch.js
+++ angular-backstretch.js
@@ -1,9 +1,42 @@
-angular.module('backstretch')
-	.directive('backstretch', function () {
-		return {
-			restrict: 'A'
-			link: function (scope, element, attr) {
-				$(element).backstretch(attr.backgroundUrl);
-			}
-		}
-	});
+/*
+ * Title: ngBackstretch
+ * Description: An angular directive to assist the use of jQuery Backstretch.
+ *              A plugin by Robbin & Co http://srobbin.com/jquery-plugins/backstretch/
+ * Version: 1.0.0
+ * Authors: Steven Collier, Erik Zettersten
+ */
+(function() {
+
+    /* Start ngBackstretch */
+
+    'use strict';
+
+    angular.module('ngBackstretch', [])
+
+    .directive('ngBackstretch', [
+
+        function() {
+
+            if (typeof $.fn.backstretch !== 'function')
+                throw new Error('ngBackstretch | Please make sure the jquery backstretch plugin is included before this directive is added.')
+
+            return {
+                restrict: 'A',
+                link: function(scope, element, attr) {
+
+                    if (attr.ngBackstretch === '' || typeof attr.ngBackstretch === 'undefined')
+                        throw new Error('ngBackstretch | You have not declared an image to be stretched.')
+
+                    if (element.context.toString().match(/HTMLBodyElement/gi).length)
+                        return $.backstretch(attr.ngBackstretch);
+
+                    $(element).backstretch(attr.ngBackstretch);
+
+                }
+            }
+
+        }
+
+    ]);
+
+}).call(this);
